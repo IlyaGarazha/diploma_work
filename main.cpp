68870b3b759e49ed2ea6e286db578d7056287f8e
Ilya <garazha.ilya.job@yandex.ru>
2021-02-13 20:47:03 +0300

    Create and copy example of face detection from https://github.com/bewagner/visuals

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <opencv4/opencv2/opencv.hpp>
+#include "FaceDetector.h"
+using namespace cv;
+
+int main(int argc, char **argv) {
+
+	VideoCapture video_capture;
+	if (!video_capture.open(0)) {
+		return 0;
+	}
+
+	FaceDetector face_detector;
+
+	Mat frame;
+	while (true) {
+		video_capture >> frame;
+
+		auto rectangles = face_detector.detect_face_rectangles(frame);
+		Scalar color(0, 105, 205);
+		for(const auto & r : rectangles){
+			rectangle(frame, r, color, 4);
+		}
+
+
+		imshow("Image", frame);
+		int esc_key = 27;
+		if (waitKey(10) == esc_key) {
+			break;
+		}
+	}
+
+	destroyAllWindows();
+	video_capture.release();
+
+	return 0;
+}
